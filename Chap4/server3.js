2669e51c869f760e20907d0bbb1498ed4867e0b3
WonjuneLee <cloudysoul@naver.com>
2019-08-29 19:42:33 +0900

    4.2 쿠키와 세션 이해하기

---
+++ Chap4/server3.js
@@ -0,0 +1,24 @@
+const http = require('http');
+
+const parseCookies = (cookie = '') => 
+    cookie
+    .split(';')
+    .map(v => v.split('='))
+    .map(([k, ... vs]) => [k, vs.join('=')])
+    .reduce((acc, [k, v]) => {
+        acc[k.trim()] = decodeURIComponent(v);
+        return acc;
+    }, {});
+
+http.createServer((req, res) => {
+    console.log(req.headers.cookie);
+    console.log(req.headers.cookie.split(';'));
+    console.log(req.headers.cookie.split(';').map(v => v.split('=')));
+    console.log(req.headers.cookie.split(';').map(v => v.split('=')).map(([k, ... vs]) => [k, vs.join('=')]));
+    const cookies = parseCookies(req.headers.cookie);
+    console.log(req.url, cookies);
+    res.writeHead(200, { 'Set-Cookie': 'mycookie=test' });
+    res.end('Hello Cookie');
+}).listen(8082, () => {
+    console.log('8082번 포트에서 서버 대기중입니다.');
+});
