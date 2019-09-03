45b0864a01e19f0e9515260fe7ee9ab01b630519
WonjuneLee <cloudysoul@naver.com>
2019-09-03 19:11:25 +0900

    6장 익스프레스 웹 서버 만들기

---
+++ chap6/learn-express/app.js
@@ -0,0 +1,49 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+// app.use(function(req, res, next) {
+//   console.log(req.url, '저도 미들웨어 입니다');
+//   next();
+// });
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+//미들웨어를 체이닝하여 연결
+//app.use('/', function (req, res, next) { console.log("첫 번째 미들웨어"); next(); }, function (req, res, next) { console.log("두 번째 미들웨어"); next(); }, indexRouter);
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
