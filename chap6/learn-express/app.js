fa2cd8e2aa03f094087c8c298b7ffc9f69995600
WonjuneLee <cloudysoul@naver.com>
2019-09-06 11:58:33 +0900

    6.3 미들웨어

--- chap6/learn-express/app.js
+++ chap6/learn-express/app.js
@@ -3,6 +3,8 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var session = require('express-session');
+var flash = require('connect-flash');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -11,7 +13,9 @@ var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+// app.set('view engine', 'pug');
+//view 엔진을 ejs로 변경 시
+app.set('view engine', 'ejs');
 
 // app.use(function(req, res, next) {
 //   console.log(req.url, '저도 미들웨어 입니다');
@@ -19,10 +23,20 @@ app.set('view engine', 'pug');
 // });
 
 app.use(logger('dev'));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({
+  resave: false,
+  saveUninitialized: false,
+  secret: 'secret code',
+  cookie: {
+    httpOnly: true,
+    secure: false,
+  },
+}));
+app.use(flash());
 
 //미들웨어를 체이닝하여 연결
 //app.use('/', function (req, res, next) { console.log("첫 번째 미들웨어"); next(); }, function (req, res, next) { console.log("두 번째 미들웨어"); next(); }, indexRouter);
