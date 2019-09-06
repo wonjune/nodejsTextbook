39254788fd95a162d4f11a895486c49964e44366
WonjuneLee <cloudysoul@naver.com>
2019-09-06 12:27:46 +0900

    6.4 Router 객체로 라우팅 분리하기

--- chap6/learn-express/routes/index.js
+++ chap6/learn-express/routes/index.js
@@ -2,8 +2,17 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+  res.render('index', {
+    title: 'Express'
+  });
+});
+
+// localhost:3000/params/1234?limit=5&skip=10 호출 시 출력 : 
+// {"id":"1234"}, {"limit":"5","skip":"10"}
+router.get('/params/:id', function (req, res) {
+  console.log(req.params, req.query);
+  res.status(200).send(`${ JSON.stringify(req.params) }, ${ JSON.stringify(req.query) }`);
 });
 
 module.exports = router;
