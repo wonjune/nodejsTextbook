819954812a65831ed371e4bee1e6a4563ab44f6b
WonjuneLee <cloudysoul@naver.com>
2019-09-17 19:22:07 +0900

    클라이언트 단 로직 추가

--- chap7/learn-sequelize/models/index.js
+++ chap7/learn-sequelize/models/index.js
@@ -39,4 +39,7 @@ db.Sequelize = Sequelize;
 db.User = require('./user')(sequelize, Sequelize);
 db.Comment = require('./comment')(sequelize, Sequelize);
 
+db.User.hasMany(db.Comment, { foreignKey: 'commenter', sourceKey: 'id' });
+db.Comment.belongsTo(db.User, { foreignKey: 'commenter', targetKey: 'id' });
+
 module.exports = db;
